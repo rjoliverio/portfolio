21f4d12b4604c45b3241ea063f5ae4159975ae7d
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-01-26 15:29:33 +0800

    [PP-1] FE - Create Landing Page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			container: {
-				center: true,
-			},
-			fontFamily: {
-				poppins: ["Poppins", "sans-serif"],
-			},
-		},
-	},
-	plugins: [],
-};
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
