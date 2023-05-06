3d3581e48c7d9faf571f5aa021c72087622b0f78
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-05-06 23:57:22 +0800

    [PP-18] Implement download resume

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_ORIGIN: process.env.NEXT_PUBLIC_API_ORIGIN,
+    NEXT_PUBLIC_API_ORIGIN: process.env.API_ORIGIN,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
