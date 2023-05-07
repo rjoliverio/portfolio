2f9fa802994888ef64faa96ecb79bd73499967f1
Rogelio John Oliverio <rogeliojohn.oliverio@sun-asterisk.com>
2023-05-07 23:10:17 +0800

    [Bugfix/PP-22] FE - Fix resume rename on download

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_ORIGIN: process.env.API_ORIGIN,
+    NEXT_PUBLIC_API_ORIGIN: process.env.NEXT_PUBLIC_API_ORIGIN,
+    NEXT_PUBLIC_CLIENT_ORIGIN: process.env.NEXT_PUBLIC_CLIENT_ORIGIN,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
